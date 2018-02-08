2fb50791f9b22effdec55004d0036398dc8bcf2e
Carlos Adolfo Ortiz Quiros (Mac) <carlosaortiz@yahoo.com>
2018-02-08 10:09:59 -0500

    Adds ASP.NET Core Web MVC project.

---
+++ app-code/microservices/user-info/user-info-webmvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace user_info_webmvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
