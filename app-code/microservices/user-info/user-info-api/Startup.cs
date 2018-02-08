bd6baffd2b754caa586653b51b87fa1a7a5f9aa4
Carlos Adolfo Ortiz Quiros (Mac) <carlosaortiz@yahoo.com>
2018-02-08 12:36:11 -0500

    Updates modification dates and formatting of files.
    Updates nugget package versions.

--- app-code/microservices/user-info/user-info-api/Startup.cs
+++ app-code/microservices/user-info/user-info-api/Startup.cs
@@ -3,7 +3,7 @@
 /* Description:   Main entry point to configure ASP.NET Core Web Api app.     */
 /* Author:        Carlos Adolfo Ortiz Quirós (COQ)                            */
 /* Date:          Feb.06/2018                                                 */
-/* Last Modified: Feb.07/2018                                                 */
+/* Last Modified: Feb.08/2018                                                 */
 /* Version:       1.1                                                         */
 /* Copyright (c), 2018 CSoftZ.                                                */
 /*----------------------------------------------------------------------------*/
@@ -12,12 +12,12 @@
  Feb.06/2018 COQ  File created.
  -----------------------------------------------------------------------------*/
 
+using CSoftZ.User.Info.Api.Services;
+using CSoftZ.User.Info.Api.Services.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using CSoftZ.User.Info.Api.Services;
-using CSoftZ.User.Info.Api.Services.Interfaces;
 
 namespace CSoftZ.User.Info.Api
 {
